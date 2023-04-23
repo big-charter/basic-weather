18ac4d68f5f983ff51d9e7f393e8c4add807f599
big-charter <bigcharter@protonmail.com>
2023-04-23 18:20:54 -0400

    Use utils class, update hourly display

--- src/client/basic-weather-client/src/components/CurrentGeneralInfo.tsx
+++ src/client/basic-weather-client/src/components/CurrentGeneralInfo.tsx
@@ -1,44 +1,21 @@
 import CurrentWeather from "@/models/CurrentWeather";
+import {
+  formatTemp,
+  timestampToDateString,
+  timestampToTimeString,
+} from "@/util/util";
+
 import React from "react";
 
 const CurrentGeneralInfo = (props: {
   zipCode: string;
   currentData: CurrentWeather;
 }) => {
-  const timestampToDateString = (UNIX_timestamp: number) => {
-    const date = new Date(UNIX_timestamp * 1000);
-    const months = [
-      "Jan",
-      "Feb",
-      "Mar",
-      "Apr",
-      "May",
-      "Jun",
-      "Jul",
-      "Aug",
-      "Sep",
-      "Oct",
-      "Nov",
-      "Dec",
-    ];
-    const year = date.getFullYear();
-    const month = months[date.getMonth()];
-    const day = date.getDate();
-    const dateString = month + " " + day + ", " + year;
-    return dateString;
-  };
-
-  const timestampToTimeString = (UNIX_timestamp: number) => {
-    return new Date(UNIX_timestamp * 1e3).toLocaleTimeString();
-  };
-
   const basicDisplay = (data: any) => {
     const currentDateString = timestampToDateString(data.dt);
     const currentTimeString = timestampToTimeString(data.dt);
     const sunriseTimeString = timestampToTimeString(data.sunrise);
     const sunsetTimeString = timestampToTimeString(data.sunset);
-    const currentTemp = Math.round(data.temp);
-    const feelsLike = Math.round(data.feels_like);
     return (
       <>
         <p>
@@ -46,8 +23,8 @@ const CurrentGeneralInfo = (props: {
         </p>
         <p>Sunrise: {sunriseTimeString}</p>
         <p>Sunset: {sunsetTimeString}</p>
-        <p>Current Temp: {currentTemp}&#8457;</p>
-        <p>Feels Like: {feelsLike}&#8457;</p>
+        <p>Current Temp: {formatTemp(data.temp)}</p>
+        <p>Feels Like: {formatTemp(data.feels_like)}</p>
       </>
     );
   };
--- src/client/basic-weather-client/src/components/HourlyForecastItem.tsx
+++ src/client/basic-weather-client/src/components/HourlyForecastItem.tsx
@@ -1,15 +1,15 @@
 import HourlyForecast from "@/models/HourlyForecast";
+import { formatTemp, formatWind, timestampToTimeString } from "@/util/util";
 import React from "react";
 
 const HourlyForecastItem = (props: { hourlyForecast: HourlyForecast }) => {
   return (
     <div className="col">
       <div className="card">
-        <div className="card-body text-dark">
-          <div>Hour</div>
-          <div>Time: {props.hourlyForecast.dt}</div>
-          <div>Temp: {props.hourlyForecast.temp}</div>
-          <div>Wind: {props.hourlyForecast.wind_speed}</div>
+        <div className="card-body text-dark text-nowrap">
+          <h5>{timestampToTimeString(props.hourlyForecast.dt)}</h5>
+          <div>{formatTemp(props.hourlyForecast.temp)}</div>
+          <div>{formatWind(props.hourlyForecast.wind_speed)}</div>
         </div>
       </div>
     </div>
