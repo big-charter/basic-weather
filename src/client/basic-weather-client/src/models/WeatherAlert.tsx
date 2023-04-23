c1e59a76a58b7ecda0ca572d8215e43c12c4805a
big-charter <bigcharter@protonmail.com>
2023-04-22 23:35:49 -0400

    Weather alert

--- src/client/basic-weather-client/src/models/WeatherAlert.tsx
+++ src/client/basic-weather-client/src/models/WeatherAlert.tsx
@@ -1,3 +1,11 @@
-type WeatherAlert = {};
+type WeatherAlert = {
+  sender_name: string;
+  event: string;
+  start: number;
+  end: number;
+  description: string;
+  // Not good documentation on this
+  tags: any;
+};
 
 export default WeatherAlert;
