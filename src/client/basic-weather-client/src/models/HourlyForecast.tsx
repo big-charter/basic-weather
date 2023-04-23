952576c87191de28f92b5437340e49a4a0468df3
big-charter <bigcharter@protonmail.com>
2023-04-22 23:35:40 -0400

    Hourly forecast

--- src/client/basic-weather-client/src/models/HourlyForecast.tsx
+++ src/client/basic-weather-client/src/models/HourlyForecast.tsx
@@ -1,3 +1,25 @@
-type HourlyForecast = {};
+type HourlyForecast = {
+  dt: number;
+  temp: number;
+  feels_like: number;
+  pressure: number;
+  humidity: number;
+  dew_point: number;
+  uvi: number;
+  clouds: number;
+  visibility: number;
+  wind_speed: number;
+  wind_deg: number;
+  wind_gust: number;
+  // WeatherCondition
+  // TODO: Breakout
+  weather: {
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+  }[];
+  pop: number;
+};
 
 export default HourlyForecast;
