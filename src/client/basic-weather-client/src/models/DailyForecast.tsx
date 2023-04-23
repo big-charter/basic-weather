7e316a06b3b32c05e2214dea3844da0dd8384b93
big-charter <bigcharter@protonmail.com>
2023-04-22 23:35:18 -0400

    Daily forecast

--- src/client/basic-weather-client/src/models/DailyForecast.tsx
+++ src/client/basic-weather-client/src/models/DailyForecast.tsx
@@ -1,3 +1,43 @@
-type DailyForecast = {};
+type DailyForecast = {
+  dt: number;
+  sunrise: number;
+  sunset: number;
+  moonrise: number;
+  moonset: number;
+  moon_phase: number;
+  // Brekaout?
+  temp: {
+    day: number;
+    min: number;
+    max: number;
+    night: number;
+    eve: number;
+    morn: number;
+  };
+  // Breakout?
+  feels_like: {
+    day: number;
+    night: number;
+    eve: number;
+    morn: number;
+  };
+  pressure: number;
+  humidity: number;
+  dew_point: number;
+  wind_speed: number;
+  wind_deg: number;
+  wind_gust: number;
+  // TODO: Breakout into WeatherCondition
+  weather: {
+    id: number;
+    main: string;
+    description: number;
+    icon: string;
+  }[];
+  clouds: number;
+  pop: number;
+  rain: number;
+  uvi: number;
+};
 
 export default DailyForecast;
