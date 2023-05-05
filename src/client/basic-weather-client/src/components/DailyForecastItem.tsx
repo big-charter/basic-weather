ff65a3e657dc0440bac17a6a3fb11e57533a9264
big-charter <bigcharter@protonmail.com>
2023-05-04 23:50:17 -0400

    Add more data to daily item

--- src/client/basic-weather-client/src/components/DailyForecastItem.tsx
+++ src/client/basic-weather-client/src/components/DailyForecastItem.tsx
@@ -1,5 +1,6 @@
 import DailyForecast from "@/models/DailyForecast";
 import {
+  formatDecimalToPercentage,
   formatTemp,
   formatWind,
   timestampToShortDateString,
@@ -9,16 +10,33 @@ import WeatherIcon from "./WeatherIcon";
 
 const DailyForecastItem = (props: { dailyForecast: DailyForecast }) => {
   return (
-    <div className="col">
-      <div className="card shadow-sm">
-        <div className="card-body text-dark">
-          <h5>{timestampToShortDateString(props.dailyForecast.dt)}</h5>
-          <WeatherIcon icon={props.dailyForecast.weather[0].icon} />
-          <div>
-            {formatTemp(props.dailyForecast.temp.min)} -{" "}
-            {formatTemp(props.dailyForecast.temp.max)}
+    <div className="card shadow-sm">
+      <div className="card-body text-dark">
+        <div className="row">
+          <div className="col">
+            <h5>{timestampToShortDateString(props.dailyForecast.dt)}</h5>
           </div>
-          <div>{formatWind(props.dailyForecast.wind_speed)}</div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <WeatherIcon icon={props.dailyForecast.weather[0].icon} />
+            <div>
+              {formatTemp(props.dailyForecast.temp.min)} -{" "}
+              {formatTemp(props.dailyForecast.temp.max)}
+            </div>
+            <div>{formatWind(props.dailyForecast.wind_speed)}</div>
+          </div>
+          <div className="col">
+            <p>
+              {formatDecimalToPercentage(props.dailyForecast.pop)} chance of
+              precipitation
+            </p>
+            <p>
+              Daytime Feels Like:{" "}
+              {formatTemp(props.dailyForecast.feels_like.day)}
+            </p>
+            <p>Humidity: {props.dailyForecast.humidity}%</p>
+          </div>
         </div>
       </div>
     </div>
--- src/client/basic-weather-client/src/components/DailyForecastList.tsx
+++ src/client/basic-weather-client/src/components/DailyForecastList.tsx
@@ -6,7 +6,11 @@ const DailyForecastList = (props: { daily: DailyForecast[] }) => {
   return (
     <div className="h-50 row row-cols-1 vertical-scrollable g-4">
       {props.daily.map((dailyForecast: DailyForecast) => {
-        return <DailyForecastItem dailyForecast={dailyForecast} />;
+        return (
+          <div className="col">
+            <DailyForecastItem dailyForecast={dailyForecast} />
+          </div>
+        );
       })}
     </div>
   );
