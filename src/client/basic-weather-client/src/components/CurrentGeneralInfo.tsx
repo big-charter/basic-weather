2864e9e40663ddc0f336250b6f7b7c223d703698
big-charter <bigcharter@protonmail.com>
2023-05-04 23:38:45 -0400

    Add more info to current weather

--- src/client/basic-weather-client/src/components/CurrentGeneralInfo.tsx
+++ src/client/basic-weather-client/src/components/CurrentGeneralInfo.tsx
@@ -7,27 +7,45 @@ import {
 } from "@/util/util";
 
 import React from "react";
+import WeatherIcon from "./WeatherIcon";
 
 const CurrentGeneralInfo = (props: {
   name: string;
   currentData: CurrentWeather;
 }) => {
-  const basicDisplay = (data: any) => {
+  const basicDisplay = (data: CurrentWeather) => {
     const currentDateString = timestampToDateString(data.dt);
     const currentTimeString = timestampToTimeString(data.dt);
     const sunriseTimeString = timestampToTimeString(data.sunrise);
     const sunsetTimeString = timestampToTimeString(data.sunset);
+    console.log(data);
     return (
-      <>
-        <p>
-          {currentDateString} {currentTimeString}
-        </p>
-        <p>Sunrise: {sunriseTimeString}</p>
-        <p>Sunset: {sunsetTimeString}</p>
-        <p>Current Temp: {formatTemp(data.temp)}</p>
-        <p>Feels Like: {formatTemp(data.feels_like)}</p>
-        <p>Wind: {formatWind(data.wind_speed)}</p>
-      </>
+      <div className="row">
+        <div className="col">
+          <p>
+            {currentDateString} {currentTimeString}
+          </p>
+          <p>
+            <WeatherIcon icon={data.weather[0].icon} />
+          </p>
+          <p>
+            {data.weather[0].main && data.weather[0].description && (
+              <p>
+                {data.weather[0].main}; {data.weather[0].description}
+              </p>
+            )}
+          </p>
+          <p>Sunrise: {sunriseTimeString}</p>
+          <p>Sunset: {sunsetTimeString}</p>
+        </div>
+        <div className="col">
+          <p>Current Temp: {formatTemp(data.temp)}</p>
+          <p>Feels Like: {formatTemp(data.feels_like)}</p>
+          <p>Humidity: {data.humidity}%</p>
+          <p>Wind: {formatWind(data.wind_speed)}</p>
+          {data.wind_gust && <p>Wind Gust: {formatWind(data.wind_gust)}</p>}
+        </div>
+      </div>
     );
   };
 
