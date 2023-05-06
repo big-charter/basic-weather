b8e8be869086c660158af3cbf0bb816e317f400f
big-charter <bigcharter@protonmail.com>
2023-05-05 21:48:41 -0400

    Refactor CurrentGeneralInfo

--- src/client/basic-weather-client/src/components/CurrentGeneralInfo.tsx
+++ src/client/basic-weather-client/src/components/CurrentGeneralInfo.tsx
@@ -13,48 +13,42 @@ const CurrentGeneralInfo = (props: {
   name: string;
   currentData: CurrentWeather;
 }) => {
-  const basicDisplay = (data: CurrentWeather) => {
-    const currentDateString = timestampToDateString(data.dt);
-    const currentTimeString = timestampToTimeString(data.dt);
-    const sunriseTimeString = timestampToTimeString(data.sunrise);
-    const sunsetTimeString = timestampToTimeString(data.sunset);
-    console.log(data);
-    return (
-      <div className="row">
-        <div className="col">
-          <p>
-            {currentDateString} {currentTimeString}
-          </p>
-          <p>
-            <WeatherIcon icon={data.weather[0].icon} />
-          </p>
-          <p>
-            {data.weather[0].main && data.weather[0].description && (
-              <p>
-                {data.weather[0].main}; {data.weather[0].description}
-              </p>
-            )}
-          </p>
-          <p>Sunrise: {sunriseTimeString}</p>
-          <p>Sunset: {sunsetTimeString}</p>
-        </div>
-        <div className="col">
-          <p>Current Temp: {formatTemp(data.temp)}</p>
-          <p>Feels Like: {formatTemp(data.feels_like)}</p>
-          <p>Humidity: {data.humidity}%</p>
-          <p>Wind: {formatWind(data.wind_speed)}</p>
-          {data.wind_gust && <p>Wind Gust: {formatWind(data.wind_gust)}</p>}
-        </div>
-      </div>
-    );
-  };
-
   return (
     <div>
       <h2>Weather forecast for {props.name}</h2>
       <div className="card mb-4 shadow-sm">
         <div className="card-body text-dark pb-0">
-          {basicDisplay(props.currentData)}
+          <div className="row">
+            <div className="col">
+              <p>
+                {timestampToDateString(props.currentData.dt)}{" "}
+                {timestampToTimeString(props.currentData.dt)}
+              </p>
+              <p>
+                <WeatherIcon icon={props.currentData.weather[0].icon} />
+              </p>
+              <p>
+                {props.currentData.weather[0].main &&
+                  props.currentData.weather[0].description && (
+                    <p>
+                      {props.currentData.weather[0].main};{" "}
+                      {props.currentData.weather[0].description}
+                    </p>
+                  )}
+              </p>
+              <p>Sunrise: {timestampToTimeString(props.currentData.sunrise)}</p>
+              <p>Sunset: {timestampToTimeString(props.currentData.sunset)}</p>
+            </div>
+            <div className="col">
+              <p>Current Temp: {formatTemp(props.currentData.temp)}</p>
+              <p>Feels Like: {formatTemp(props.currentData.feels_like)}</p>
+              <p>Humidity: {props.currentData.humidity}%</p>
+              <p>Wind: {formatWind(props.currentData.wind_speed)}</p>
+              {props.currentData.wind_gust && (
+                <p>Wind Gust: {formatWind(props.currentData.wind_gust)}</p>
+              )}
+            </div>
+          </div>
         </div>
       </div>
     </div>
