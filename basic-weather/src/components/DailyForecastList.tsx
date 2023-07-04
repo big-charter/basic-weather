898d3d8a60b94d613a9f9d62cb127bc50a46630b
big-charter <bigcharter@protonmail.com>
2023-07-04 12:08:09 -0400

    Better responsiveness

--- basic-weather/src/components/DailyForecastList.tsx
+++ basic-weather/src/components/DailyForecastList.tsx
@@ -4,10 +4,10 @@ import DailyForecastItem from "./DailyForecastItem";
 
 const DailyForecastList = (props: { daily: DailyForecast[] }) => {
   return (
-    <div className="h-50 row row-cols-1 vertical-scrollable g-4">
+    <div className="h-50 row row-cols-1 vertical-scrollable g-4 mt-4 mt-sm-0">
       {props.daily.map((dailyForecast: DailyForecast, index: number) => {
         return (
-          <div key={index} className="col">
+          <div key={index} className={`col ${index == 0 ? 'mt-0' : ''}`}>
             <DailyForecastItem dailyForecast={dailyForecast} />
           </div>
         );
