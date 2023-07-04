69b1ad6664728bc34107f24857822d281f1391d6
big-charter <bigcharter@protonmail.com>
2023-07-04 13:22:56 -0400

    Add prettier and format all files

--- basic-weather/src/components/DailyForecastList.tsx
+++ basic-weather/src/components/DailyForecastList.tsx
@@ -7,7 +7,7 @@ const DailyForecastList = (props: { daily: DailyForecast[] }) => {
     <div className="h-50 row row-cols-1 vertical-scrollable g-4 mt-4 mt-sm-0">
       {props.daily.map((dailyForecast: DailyForecast, index: number) => {
         return (
-          <div key={index} className={`col ${index == 0 ? 'mt-0' : ''}`}>
+          <div key={index} className={`col ${index == 0 ? "mt-0" : ""}`}>
             <DailyForecastItem dailyForecast={dailyForecast} />
           </div>
         );
